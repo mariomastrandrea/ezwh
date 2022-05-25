97f3fc6ae1db3e453d71fcaa20539d38ad4702f3
KRISTI GJERKO s301101 <s301101@studenti.polito.it>
2022-05-25 16:32:21 +0200

    fixed insertSamples

--- code/server/db/dbInit.sql
+++ code/server/db/dbInit.sql
@@ -69,7 +69,7 @@ VALUES ('12345678901234567890123456789011',10.99,'a sku',1,1),
 INSERT INTO Sku (Price,AvailableQuantity,Position,Notes,Volume,Weight,Description,ID)
 VALUES (10.99,2,800234523417,'first sku',50,100,'a sku',1),
        (10.99,0,800234523415,'third sku',60,101,'another sku',3),
-       (10.99,1,800234523412,'second sku',5,10,'another sku',5);
+       (10.99,1,800234523413,'second sku',5,10,'another sku',5);
 
 INSERT INTO SkuItem (DateOfStock, Available, SkuId, RFID)
 VALUES ('2021/11/29', 1, 1, '12345678901234567890123456789011'),
