91741707008689b8823f8ec4fb79ee7cdae155c0
Mario Mastrandrea s302219 <s302219@studenti.polito.it>
2022-05-25 19:46:49 +0200

    Finished sku api tests

--- code/server/test/testSkuRouter.js
+++ code/server/test/testSkuRouter.js
@@ -6,15 +6,31 @@ chai.should();
 const app = require('../server');
 var agent = chai.request.agent(app);
 
+const { postPosition } = require("./testPositionsRouter");
+
 describe('Sku API tests', () => {
-    postSkuTest(201, {
-        description: "a new sku",
+    deleteAll(200);
+
+    const newSku1 = {
+        description: "first sku",
         weight: 100,
         volume: 50,
-        notes: "SKU",
+        notes: "1st SKU",
         price: 10.99,
         availableQuantity: 50
-    });
+    };
+
+    const newSku2 = {
+        description: "second sku",
+        weight: 100,
+        volume: 50,
+        notes: "2nd SKU",
+        price: 10.99,
+        availableQuantity: 20
+    }
+
+    postSkuTest(201, newSku1);
+    postSkuTest(201, newSku2);
 
     // unprocessable body
     postSkuTest(422, {
@@ -23,49 +39,93 @@ describe('Sku API tests', () => {
         volume: "fake volume",
         notes: "SKU",
         price: 10.99,
-        availableQuantity : 0
+        availableQuantity: 0
+    });
+    postSkuTest(422, {
+        description: "a new sku",
+        weight: "fake weight",
+        notes: "SKU",
+        price: 10.99,
     });
 
-    getAllSkuTest(200);
-    getSkuByIdTest(200, 1);
+    getAllSkuTest(200, [{ ...newSku1, id: 1, position: "", testDescriptors: [] }, { ...newSku2, id: 2, position: "", testDescriptors: [] }]);
+    getSkuByIdTest(200, 1, { ...newSku1, id: 1, position: "", testDescriptors: [] });
     getSkuByIdTest(404, 999);
+
+    putSkuTest(422, 1);
+    putSkuTest(422, 1, {
+        "newDescription": "sdafegrd",
+        "newWeight": 1000,
+        "newVolume": 5000,
+        "newNotes": "adsfesg",
+    });
+
+    putSkuTest(404, 999, {
+        "newDescription": "sdafegrd",
+        "newWeight": 1000,
+        "newVolume": 5000,
+        "newNotes": "adsfesg",
+        "newPrice": 101.99,
+        "newAvailableQuantity": 50
+    });
+
+    putSkuTest(200, 1, {
+        "newDescription": "a new first sku",
+        "newWeight": 10,
+        "newVolume": 50,
+        "newNotes": "modified first SKU",
+        "newPrice": 101.99,
+        "newAvailableQuantity": 5
+    })
+
+    getSkuByIdTest(200, 1, {
+        "id": 1,
+        "position": "",
+        "description": "a new first sku",
+        "weight": 10,
+        "volume": 50,
+        "notes": "modified first SKU",
+        "price": 101.99,
+        "availableQuantity": 5,
+        "testDescriptors": []
+    });
+
+    postPosition(201, "123412341234", "1234", "1234", "1234", 999, 999);
+
+    putSkuPositionTest(200, 1, {
+        position: "123412341234"
+    })
+
+    getSkuByIdTest(200, 1, {
+        "id": 1,
+        "position": "123412341234",
+        "description": "a new first sku",
+        "weight": 10,
+        "volume": 50,
+        "notes": "modified first SKU",
+        "price": 101.99,
+        "availableQuantity": 5,
+        "testDescriptors": []
+    });
+
+    deleteSkuTest(422, 999);
+    deleteSkuTest(204, 1);
+    deleteSkuTest(422, 1);
+    getSkuByIdTest(404, 1);
+
+    deleteAll(200);
 });
 
 //#region Sku test functions
-function getAllSkuTest(expectedHttpStatus) {
-    it('get all skus', function(done) {
+function getAllSkuTest(expectedHttpStatus, expectedResponseBody) {
+    it('get all skus', function (done) {
         agent.get("/api/skus")
-            .then(function(res) {
+            .then(function (res) {
                 res.should.have.status(expectedHttpStatus);
                 res.body.should.be.an('array');
 
-                for(let sku of res.body) {
-                    sku.id.should.be.a('number');
-                    sku.id.should.be.greaterThan(0);
-
-                    sku.description.should.be.a('string');
-                    sku.description.should.exist;
-
-                    sku.weight.should.be.a('number');
-                    sku.weight.should.be.greaterThanOrEqual(0);
-
-                    sku.volume.should.be.a('number');
-                    sku.volume.should.be.greaterThanOrEqual(0);
-
-                    sku.notes.should.be.a('string');
-                    sku.notes.should.exist;
-
-                    sku.position.should.be.a('string');
-                    sku.position.should.have.lengthOf(12);
-
-                    sku.availableQuantity.should.be.a('number');
-                    sku.availableQuantity.should.be.greaterThanOrEqual(0);
-
-                    sku.price.should.be.a('number');
-                    sku.price.should.be.greaterThanOrEqual(0);
-
-                    sku.testDescriptors.should.be.an('array');
-                }
+                if (expectedHttpStatus === 200)
+                    res.body.should.be.deep.equal(expectedResponseBody);
 
                 done();
             })
@@ -73,38 +133,14 @@ function getAllSkuTest(expectedHttpStatus) {
     });
 }
 
-function getSkuByIdTest(expectedHttpStatus, skuId) {
-    it('get sku by id', function(done) {
+function getSkuByIdTest(expectedHttpStatus, skuId, expectedResponseBody) {
+    it('get sku by id', function (done) {
         agent.get(`/api/skus/${skuId}`)
-            .then(function(res) {
+            .then(function (res) {
                 res.should.have.status(expectedHttpStatus);
 
-                if(expectedHttpStatus === 200) {
-                    const sku = res.body;
-                    
-                    sku.description.should.be.a('string');
-                    sku.description.should.exist;
-
-                    sku.weight.should.be.a('number');
-                    sku.weight.should.be.greaterThanOrEqual(0);
-
-                    sku.volume.should.be.a('number');
-                    sku.volume.should.be.greaterThanOrEqual(0);
-
-                    sku.notes.should.be.a('string');
-                    sku.notes.should.exist;
-
-                    sku.position.should.be.a('string');
-                    sku.position.should.have.lengthOf(12);
-
-                    sku.availableQuantity.should.be.a('number');
-                    sku.availableQuantity.should.be.greaterThanOrEqual(0);
-
-                    sku.price.should.be.a('number');
-                    sku.price.should.be.greaterThanOrEqual(0);
-
-                    sku.testDescriptors.should.be.an('array');
-                }
+                if (expectedHttpStatus === 200)
+                    res.body.should.be.deep.equal(expectedResponseBody);
 
                 done();
             })
@@ -113,10 +149,10 @@ function getSkuByIdTest(expectedHttpStatus, skuId) {
 }
 
 function postSkuTest(expectedHttpStatus, requestBody) {
-    it('create sku', function(done) {
+    it('create sku', function (done) {
         agent.post("/api/sku")
             .send(requestBody)
-            .then(function(res) {
+            .then(function (res) {
                 res.should.have.status(expectedHttpStatus);
                 done();
             })
@@ -124,5 +160,57 @@ function postSkuTest(expectedHttpStatus, requestBody) {
     });
 }
 
+function putSkuTest(expectedHttpStatus, skuId, requestBody) {
+    it('update sku', function (done) {
+        agent.put(`/api/sku/${skuId}`)
+            .send(requestBody)
+            .then(function (res) {
+                res.should.have.status(expectedHttpStatus);
+                done();
+            })
+            .catch(err => done(err));
+    });
+}
 
+function putSkuPositionTest(expectedHttpStatus, skuId, requestBody) {
+    it('update sku position', function (done) {
+        agent.put(`/api/sku/${skuId}/position`)
+            .send(requestBody)
+            .then(function (res) {
+                res.should.have.status(expectedHttpStatus);
+                done();
+            })
+            .catch(err => done(err));
+    });
+}
+
+function deleteSkuTest(expectedHttpStatus, skuId) {
+    it('delete sku', function (done) {
+        agent.delete(`/api/skus/${skuId}`)
+            .then(function (res) {
+                res.should.have.status(expectedHttpStatus);
+                done();
+            })
+            .catch(err => done(err));
+    });
+}
 //#endregion
+
+function deleteAll(expectedHttpStatus) {
+    it('deleting data', function (done) {
+        agent.get('/test/deleteAll')
+            .then(function (res) {
+                res.should.have.status(expectedHttpStatus);
+                done();
+            }).catch(err => done(err));
+    });
+}
+
+module.exports = {
+    getAllSkuTest,
+    getSkuByIdTest,
+    postSkuTest,
+    putSkuTest,
+    putSkuPositionTest,
+    deleteSkuTest
+}
--- code/server/test/testTestsRouter.js
+++ code/server/test/testTestsRouter.js
@@ -59,9 +59,6 @@ function getTestDesc(expectedHTTPStatus, id, name, procedureDescription, idSKU) 
                     done();
                 });
         }
-
-
-
     });
 }
 
